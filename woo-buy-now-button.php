a92e585739f2497db308c3a0151950fea21ef012
Ainul islam <aimctgbd@gmail.com>
2025-09-06 19:11:50 +0400

    Delete woo-buy-now-button.php

--- woo-buy-now-button.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Plugin Name: WooCommerce Buy Now Button (Blocks + Single Product)
-Description: Adds Buy Now button in WooCommerce grids and single product pages. Admin can edit labels, colors, position.
-Version: 1.6
-Author: Ainul islam
-License: GPL2
-*/
